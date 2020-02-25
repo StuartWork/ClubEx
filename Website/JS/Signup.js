56844ce77387a6adf8eefa9b18e1e9c151a1ba44
Stuart Work <50558395+StuartWork@users.noreply.github.com>
2020-02-26 12:21:36 +1300

    fix tick box validation

--- Website/JS/Signup.js
+++ Website/JS/Signup.js
@@ -1,3 +1,37 @@
+window.onload = init;
+
+function init() {
+	
+	//Event for submit button
+	var formsubmit = document.getElementById("submit");
+	if (formsubmit) {
+		formsubmit.onclick = checkForm;
+		}
+}
+
+function checkForm() {	
+	
+	//gets input elelments
+	var inputs = document.getElementsByTagName('input');
+	// setting up var for check box and radio validation
+	var checkedCB=false,checkedRadio=false;
+	
+	for (var i = 0; i < inputs.length; i++) {
+		if (inputs[i].type == 'checkbox' && inputs[i].checked) {
+            checkedCB=true;
+		}else if(inputs[i].type == 'radio' && inputs[i].checked) {
+			checkedRadio=true;
+		}
+		//as long as one is true and the form is filled out it will move to the next page
+		//if there is a subsription then its true and moves to next page
+		if(checkedRadio){ return true;}
+		// if you tick your a gym member then its true and moves to next page
+		if(checkedCB){ return true;}
+  }
+  //message will pop up with an okay button if you havent picked a plan or gym member
+  alert("You must select a subscription if not a gym member.");
+  return false;
+}
 // Disable form submissions if there are invalid fields
 (function() {
   'use strict';
